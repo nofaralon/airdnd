9f6b890dff6702dbf28201e674a41f4cdc455bfb
Nofar Alon <nofaralon28@gmail.com>
2021-12-02 09:00:34 +0200

    details

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store/index.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import "./style/style.scss";
+import locale from 'element-ui/lib/locale/lang/en'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI, { locale })
 
 new Vue({
     router,
--- src/pages/stay-details.vue
+++ src/pages/stay-details.vue
@@ -3,14 +3,79 @@
     <h1>{{ stay.name }}</h1>
     <a href="">{{ stay.loc.address }}</a>
     <div class="img-container">
-      <img  v-for="(img, index) in imgs" :key="index" :src="img" alt="">
+      <img v-for="(img, index) in imgs" :key="index" :src="img" alt="" />
     </div>
+    <section class="info-container">
+      <div class="stay-info">
+        <div class="info-header">
+          <div>
+            <h2>{{ stay.type }} hosted by {{ stay.host.fullname }}</h2>
+            <span
+              >{{ stay.capacity }} Guests • {{ stay.bedroom }} Bedrooms •
+              {{ stay.beds }} Beds • {{ stay.bathrooms }} Baths
+            </span>
+          </div>
+          <img :src="`${stay.host.imgUrl}`" />
+        </div>
 
-    <div class="stay-info">
-      
-    </div>
+        <div class="main-info">
+          <div>
+            <h3>Entire Place</h3>
+            <span>You'll have the place to yourself.</span>
+          </div>
 
+          <div>
+            <h3>House Rules</h3>
+            <span>
+              This place isn’t suitable for children under 12 and the host
+              doesn’t allow pets.
+            </span>
+          </div>
 
+          <div>
+            <h3>{{ stay.host.fullname }} Is A Superhost</h3>
+            <span>
+              Superhosts are experienced, highly rated hosts who are committed
+              to providing great stays for guests.
+            </span>
+          </div>
+
+          <div>
+            <h3>Wifi</h3>
+            <span>Guests often search for this popular amenity.</span>
+          </div>
+
+          <div class="description">
+            <h2>Description</h2>
+            <p>{{ stay.summary }}</p>
+          </div>
+
+          <div class="amenities">
+            <h2>Amenities</h2>
+            <ul v-for="(amenity, index) in stay.amenities" :key="index">
+              <li>{{ amenity }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="order-form">
+        <div>
+          <p>$ {{ stay.price }}/night</p>
+          <span>4.39 <a>(3 reviews)</a></span>
+        </div>
+
+        <div class="block">
+          <el-date-picker
+            v-model="dateVal"
+            type="daterange"
+            start-placeholder="CHECK-IN"
+            end-placeholder="CHECK-OUT"
+          >
+          </el-date-picker>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
@@ -21,7 +86,8 @@ export default {
   data() {
     return {
       stay: null,
-      imgs:[]
+      imgs: [],
+      dateVal: null
     };
   },
   created() {
@@ -39,11 +105,10 @@ export default {
     },
     imgForDisplay() {
       var imgs = this.stay.imgUrls.slice(0, 5);
-      this.imgs = imgs    
-    }
+      this.imgs = imgs;
+    },
   },
-  computed: {
-  },
+  computed: {},
   components: {},
 };
 </script>
