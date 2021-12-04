dd878d1c2ad6766b0faf0b9d86a27453d574233b
Nofar Alon <nofaralon28@gmail.com>
2021-12-04 21:57:30 +0200

    page details after css changes

--- src/pages/stay-details.vue
+++ src/pages/stay-details.vue
@@ -1,6 +1,7 @@
 <template>
   <section v-if="stay" class="stay-details details-layout">
     <h1>{{ stay.name }}</h1>
+
     <div class="detsils-header">
       <a href="">{{ stay.loc.address }}</a>
       <span>
@@ -44,11 +45,12 @@
     <div class="img-container">
       <img v-for="(img, index) in imgs" :key="index" :src="img" alt="" />
     </div>
+
     <section class="info-container">
       <div class="stay-info">
         <div class="info-header">
           <div>
-            <h2>{{ stay.type }} hosted by {{ stay.host.fullname }}</h2>
+            <h2><span class="stay-name">{{ stay.type }}</span> hosted by {{ stay.host.fullname }}</h2>
             <span
               >{{ stay.capacity }} Guests • {{ stay.bedroom }}
               {{ setBedrooms }} • {{ stay.beds }} {{ setBeds }} •
@@ -79,7 +81,7 @@
                 </svg>
               </div>
               <div>
-                <h3>Entire Place</h3>
+                <h3>Entire place</h3>
                 <span>You'll have the place to yourself.</span>
               </div>
             </div>
@@ -103,7 +105,7 @@
                 </svg>
               </div>
               <div>
-                <h3>House Rules</h3>
+                <h3>House rules</h3>
                 <span>
                   This place isn’t suitable for children under 12 and the host
                   doesn’t allow pets.
@@ -130,7 +132,7 @@
                 </svg>
               </div>
               <div>
-                <h3>{{ stay.host.fullname }} Is A Superhost</h3>
+                <h3>{{ stay.host.fullname }} is a superhost</h3>
                 <span>
                   Superhosts are experienced, highly rated hosts who are
                   committed to providing great stays for guests.
@@ -304,97 +306,7 @@
         </div>
       </div>
 
-      <div class="order-form">
-        <div class="order-price">
-          <p>
-            <span>{{ stayPrice }}</span
-            >/night
-          </p>
-
-          <span>
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="star"
-              class="svg-inline--fa fa-star fa-w-18"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 576 512"
-            >
-              <path
-                fill="currentColor"
-                d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
-              ></path>
-            </svg>
-            4.39 <a>({{ stay.reviews.length }} {{ setReviews }})</a></span
-          >
-        </div>
-
-        <div class="order-picker">
-          <div class="block">
-            <div class="check">
-              <div>CHECK-IN</div>
-              <div>CHECK-OUT</div>
-            </div>
-            <el-date-picker v-model="order.Dates" type="daterange">
-            </el-date-picker>
-          </div>
-          <div @click="toggleModal">
-            <h2>GUESTS</h2>
-            <span> {{ order.guests }} </span>
-          </div>
-        </div>
-
-        <div v-if="openModal" class="guest-modal">
-          <div class="nof">
-            <div>
-              <h2>Adults</h2>
-              <span>Age 13+</span>
-            </div>
-            <div>
-              <button @click="setCountAdults('down')">-</button>
-              <span class="guests">{{ order.adults }}</span>
-              <button @click="setCountAdults('up')">+</button>
-            </div>
-          </div>
-          <div>
-            <div>
-              <h2>Kids</h2>
-              <span>Ages 2–12</span>
-            </div>
-            <div>
-              <button @click="setCountKids('down')">-</button>
-              <span class="guests">{{ order.kids }}</span>
-              <button @click="setCountKids('up')">+</button>
-            </div>
-          </div>
-        </div>
-
-        <button @click="check" class="reserve">{{checkBtn}}</button>
-
-        <div v-if="checked" class="total-order">
-          <p>You won't be charged yet</p>
-
-          <div>
-            <span>{{ stayPrice }} x {{setTotalDays}} nights</span>
-            <span>{{ nightPrice }}</span>
-          </div>
-          <div>
-            <span>Cleaning fee</span>
-            <span> ${{ order.cleaning }}</span>
-          </div>
-          <div>
-            <span>Service fee</span>
-            <span>${{ order.service }}</span>
-          </div>
-          <div>
-            <span>Total</span>
-            <span>{{ setTotalPrice }}</span>
-          </div>
-        </div>
-      </div>
-      <a>Report this listing</a>
+      <order-form @check="check" :stay="stay" :order="order"></order-form>
     </section>
 
     <section class="reviews">
@@ -474,18 +386,18 @@
 
 <script>
 import GmapMap from "../cmps/map-details.vue";
+import orderForm from "../cmps/order-form.vue";
+
 import { orderService } from "../services/order.service";
 
 export default {
   name: "stay-details",
   data() {
     return {
-      checked: false,
       stay: null,
       imgs: [],
       amenities: null,
       order: {},
-      openModal: false,
     };
   },
   created() {
@@ -506,31 +418,6 @@ export default {
       var imgs = this.stay.imgUrls.slice(0, 5);
       this.imgs = imgs;
     },
-    toggleModal() {
-      this.openModal = !this.openModal;
-    },
-    setCountAdults(val) {
-      if (val === "down") {
-        if (this.order.adults === 0) return;
-        this.order.adults -= 1;
-        this.order.guests -= 1;
-      } else {
-        if (this.order.guests === this.stay.capacity) return;
-        this.order.adults += 1;
-        this.order.guests += 1;
-      }
-    },
-    setCountKids(val) {
-      if (val === "down") {
-        if (this.order.kids === 0) return;
-        this.order.kids -= 1;
-        this.order.guests -= 1;
-      } else {
-        if (this.order.guests === this.stay.capacity) return;
-        this.order.kids += 1;
-        this.order.guests += 1;
-      }
-    },
     randomDate(start, end, startHour, endHour) {
       var date = new Date(+start + Math.random() * (end - start));
       var hour = (startHour + Math.random() * (endHour - startHour)) | 0;
@@ -539,17 +426,11 @@ export default {
       return sortDate;
     },
     check() {
-      this.checked = true;
-
-      if (this.checked){
-        const {_id, name, price} = this.stay
-        this.order.stay = {_id, name, price}
-        this.order.hostId = this.stay.host._id
-        console.log(this.order.stay);
-          this.$store.dispatch({ type: "addOrder", order: this.order })
-      }
-
-    
+      const { _id, name, price } = this.stay;
+      this.order.stay = { _id, name, price };
+      this.order.hostId = this.stay.host._id;
+      console.log(this.order.stay);
+      this.$store.dispatch({ type: "addOrder", order: { ...this.order } });
     },
   },
   computed: {
@@ -576,37 +457,10 @@ export default {
       if (this.stay.bathrooms > 1) return "baths";
       else return "bath";
     },
-    setTotalPrice() {
-      var total = this.order.cleaning + this.order.service + (this.order.totalDays * this.stay.price);
-      return total.toLocaleString("en-US", {
-        currency: "USD",
-        style: "currency",
-        maximumFractionDigits: 0,
-      });
-    },
-    setTotalDays(){
-      const date1 = this.order.Dates[0]
-      const date2 = this.order.Dates[1]
-      const diffTime = Math.abs(date2 - date1);
-      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-      this.order.totalDays = diffDays
-      return diffDays
-    },
-    nightPrice(){
-      var total = this.order.totalDays * this.stay.price
-      return total.toLocaleString("en-US", {
-        currency: "USD",
-        style: "currency",
-        maximumFractionDigits: 0,
-      })
-    },
-    checkBtn(){
-      if(this.checked) return 'Reserve'
-      else return 'Check availability'
-    }
   },
   components: {
     GmapMap,
+    orderForm,
   },
 };
 </script>
